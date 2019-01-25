c5a3a076d575a31a563b5c730fc1256424688ee6
Villies Thekkekara <villies.thekkekara@narvar.com>
2019-01-25 17:28:06 +0530

    Removing need for gcloud creds

--- etc/sftp.d/gcs-mounts.sh
+++ etc/sftp.d/gcs-mounts.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # File mounted as: /etc/sftp.d/mount_user_directories.sh
 
-runuser -l partner1 -c \
-'export GOOGLE_APPLICATION_CREDENTIALS=/credentials/gcloud-key.json && \
-gcsfuse -o nonempty --only-dir user1 bucket /home/user1/ftp'
+runuser -l user1 -c 'gcsfuse -o nonempty --only-dir user1 sftp-villies-test /home/user1/ftp'
 
-runuser -l partner2 -c \
-'export GOOGLE_APPLICATION_CREDENTIALS=/credentials/gcloud-key.json && \
-gcsfuse -o nonempty --only-dir user2 bucket /home/user2/ftp'
+runuser -l user2 -c 'gcsfuse -o nonempty --only-dir user2 sftp-villies-test /home/user2/ftp'
